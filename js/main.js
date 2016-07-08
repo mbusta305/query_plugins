ade4e60846e45c661e4653428bd1ca7bb0a823d1
mbusta305 <busta2356@gmail.com>
2016-07-08 12:55:48 -0400

    stick email on scroll

--- js/main.js
+++ js/main.js
@@ -8,4 +8,24 @@ $('.animsition').animsition({
 
 $('.header').sticky({
   topSpacing: 0,
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+
+$('.header').on('sticky-start', function () {
+    $('.description').html('We Build <strong>Great</strong> Apps');
+});
+
+$('.header').on('sticky-end', function () {
+  $('.description').html('We Build Apps');
+});
+
+$('.grid-full h5').sticky({
+  topSpacing: 64,
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+
+$('.grid-full h5').on('sticky-start', function(){
+  $(this).html('Want us to work on your project? <a href="mailto:email@website.com" class="email-text">Email&nbsp;us</a>');
 });
