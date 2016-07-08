3d56f07933ade0f078c937fec64b1534adb18fc5
mbusta305 <busta2356@gmail.com>
2016-07-08 12:10:26 -0400

    sticky plugin/ stick header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-$(".animsition").animsition({
+$('.animsition').animsition({
   inClass: 'fade-in-right-lg',
   outClass: 'fade-out-right-lg',
   linkElement: 'header a',
   inDuration: 1000,
   outDuration: 500,
+});
+
+$('.header').sticky({
+  topSpacing: 0,
 });
